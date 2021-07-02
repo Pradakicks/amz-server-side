cde79443909233f4386a31848cf721dcb83a08b0
Adrian <adriancleve1@gmail.com>
2021-07-02 18:28:40 -0400

    f

--- GoMonitors/main.go
+++ GoMonitors/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	fmt.Println("Testingz")
 	go Amz.NewAmzMonitor("B08G9NKWR9", `Wr0RFj%2BisP6XDoYX7Pa9cykvPmqXPMBz2xsQfKuEDYFmtOC5nhLwdsyiTNEO21AnYke1zdAAU3Vw211fnIBH%2FjR5Dx%2FNsPiNq9zeu6NRq1aFc0dqh5Tsuaa3w7GDdk0Fq57g3QXpbBmx2Ry%2FzqIsTA%3D%3D`, "GPU")
-	go Amz.NewAmzMonitor("B08R81J62G", `dPIvIYRHvMgp7%2BKcLoVDXcE8qpp13AKCYwruZDDqR39vETkAKxHMkCUFsl3kyw%2F04zX2f4RprUI7BVuqL%2F4jozYXu6g5wHuk5JIw%2FG%2FRDF7zUXZsZREGGBtKeZC4Fh0llYVk1nywlWsUd%2FkIRglNAw%3D%3D`, "GPU")
+	Amz.NewAmzMonitor("B08R81J62G", `dPIvIYRHvMgp7%2BKcLoVDXcE8qpp13AKCYwruZDDqR39vETkAKxHMkCUFsl3kyw%2F04zX2f4RprUI7BVuqL%2F4jozYXu6g5wHuk5JIw%2FG%2FRDF7zUXZsZREGGBtKeZC4Fh0llYVk1nywlWsUd%2FkIRglNAw%3D%3D`, "GPU")
 	fmt.Scanln()
 }
 func f() {
