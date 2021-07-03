4e47483784880ced9366abb2b636aa1968501eb7
Adrian <adriancleve1@gmail.com>
2021-07-02 22:14:02 -0400

    Proxy Support

--- GoMonitors/amz/amz.go
+++ GoMonitors/amz/amz.go
@@ -1,24 +1,29 @@
 package Amz
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 	"time"
 )
 
 type AmzMonitor struct {
-	Asin      string
-	OfferId   string
-	Category  string
-	url       string
-	payload   *strings.Reader
-	available bool
-	Client    http.Client
+	Asin       string
+	OfferId    string
+	Category   string
+	url        string
+	payload    *strings.Reader
+	available  bool
+	Client     http.Client
+	proxyList  []string
+	proxyCount int
 }
 
 type postData struct {
@@ -26,6 +31,12 @@ type postData struct {
 	OfferId  string
 	Category string
 	Time     int64
+}
+type Proxy struct {
+	ip       string
+	port     string
+	userAuth string
+	userPass string
 }
 
 func NewAmzMonitor(asin string, offerId string, category string) {
@@ -35,18 +46,53 @@ func NewAmzMonitor(asin string, offerId string, category string) {
 	m.OfferId = offerId
 	m.url = "https://www.amazon.com/gp/product/features/dp-fast-track/udp-ajax-handler/get-quantity-update-message.html?ie=UTF8"
 	m.available = false
+	m.Client = http.Client{Timeout: 10 * time.Second}
+	file, err := os.Open("proxy.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		m.proxyList = append(m.proxyList, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 	for {
 		m.monitor()
 	}
 }
-
+func (m *AmzMonitor) getProxy(proxyList []string) string {
+	if m.proxyCount+1 == len(proxyList) {
+		m.proxyCount = 0
+	}
+	m.proxyCount++
+	return proxyList[m.proxyCount]
+}
 func (m *AmzMonitor) monitor() {
-	proxyUrl, _ := url.Parse("http://bC19Ezqr:9P3Z7SACZG3WRGIaA7IwMeZviSojEWJhnRUpZkcj3EqylUCzZjJtu0BKr3BdPcczzFDbT-wbNpUuNu@ustr24.resi.reddirt.io:42626")
-	myClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+
+	currentProxy := m.getProxy(m.proxyList)
+	splittedProxy := strings.Split(currentProxy, ":")
+	proxy := Proxy{splittedProxy[0], splittedProxy[1], splittedProxy[2], splittedProxy[3]}
+	prox1y := fmt.Sprintf("http://%s:%s@%s:%s", proxy.userAuth, proxy.userPass, proxy.ip, proxy.port)
+	proxyUrl, err := url.Parse(prox1y)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defaultTransport := &http.Transport{
+		Proxy: http.ProxyURL(proxyUrl),
+	}
+	m.Client.Transport = defaultTransport
 
 	var currentAvailability bool
 
-	req, _ := http.NewRequest("POST", m.url, strings.NewReader(fmt.Sprintf("asin=%s&quantity=1&merchantId=A3KJ3YGUCB8ID4", m.Asin)))
+	req, err := http.NewRequest("POST", m.url, strings.NewReader(fmt.Sprintf("asin=%s&quantity=1&merchantId=A3KJ3YGUCB8ID4", m.Asin)))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	req.Header.Add("authority", "www.amazon.com")
 	req.Header.Add("sec-ch-ua", `" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"`)
 	req.Header.Add("dnt", "1")
@@ -63,10 +109,21 @@ func (m *AmzMonitor) monitor() {
 	req.Header.Add("sec-fetch-mode", "cors")
 	req.Header.Add("sec-fetch-dest", "empty")
 	req.Header.Add("accept-language", "en-US,en;q=0.9")
+	req.Header.Set("Connection", "close")
+	req.Close = true
 
-	res, _ := myClient.Do(req)
+	res, err := m.Client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	defer func() {
 		test := fmt.Sprintf("Status Code : %d Asin : %s Body Length : %d Availability : %t Current : %t", res.StatusCode, m.Asin, len(string(body)), m.available, currentAvailability)
 		fmt.Println(test)
